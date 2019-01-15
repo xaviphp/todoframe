9e554ed4a96a1c36f51efd0bd6b8dc49f61aae4e
xaviphp <mrxavi96@gmail.com>
2019-01-15 09:21:49 +0100

    probando

--- lib/init.php
+++ lib/init.php
@@ -1,9 +1,9 @@
 <?php
   session_start();
   ini_set('display_errors','1');
-  
 
-  $conn = new mysqli("localhost", "xlaguens_xavi", "xaclag", "xlaguens_todo");
+
+  $conn = new mysqli("localhost", "xlaguens_xavi", "123123", "xlaguens_todo");
 
   if ($conn->connect_errno) {
       echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
